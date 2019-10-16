1d06a9ed192160629d555eda547676bd899c2b2b
yuldashev-291286 <yuldashevruslan291286@gmail.com>
2019-10-16 22:46:08 +0300

    Проект "отвязан" от репозитория Git

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     System.out.println("Hello word !");
   }
-  
+
 }
