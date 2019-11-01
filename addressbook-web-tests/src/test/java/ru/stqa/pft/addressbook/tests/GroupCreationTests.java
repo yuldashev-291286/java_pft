ad6166d593b758c92b646cb4c9c251a94aef72a3
yuldashev-291286 <yuldashevruslan291286@gmail.com>
2019-11-01 21:10:23 +0300

    Реализованы проверки сравнения списков в тестах для создания, удаления и модификации групп.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,17 +1,29 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrimeFast() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+  @Test (enabled = false)
+  public void testPrimeLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+
+  @Test
+  public void testNonPrime() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+
+}
