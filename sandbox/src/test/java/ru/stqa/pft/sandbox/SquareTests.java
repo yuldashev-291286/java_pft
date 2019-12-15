40ad535b530aa29cf9f693c9faa637324d736a1d
yuldashev-291286 <yuldashevruslan291286@gmail.com>
2019-12-15 22:23:35 +0300

    Сломан тест для проверки Jenkins

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -8,7 +8,7 @@ public class SquareTests {
   @Test
   public void testArea(){
     Square s = new Square(5);
-    Assert.assertEquals(s.area(), 25);
+    Assert.assertEquals(s.area(), 20);
   }
 
 }
