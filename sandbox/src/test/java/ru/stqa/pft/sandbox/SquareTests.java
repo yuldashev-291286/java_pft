02ecc0e46d6a04b586aa89283fb73b406a51e5e2
yuldashev-291286 <yuldashevruslan291286@gmail.com>
2019-12-15 22:31:11 +0300

    Проверка Jenkins на исправленном тесте

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -8,7 +8,7 @@ public class SquareTests {
   @Test
   public void testArea(){
     Square s = new Square(5);
-    Assert.assertEquals(s.area(), 23);
+    Assert.assertEquals(s.area(), 25);
   }
 
 }
