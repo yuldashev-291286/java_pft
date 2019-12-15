1f4f0c29fc026eeb4d60103b38ee16ce80d09f31
yuldashev-291286 <yuldashevruslan291286@gmail.com>
2019-12-15 22:29:45 +0300

    Запуск сломанного теста на Jenkins

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -8,7 +8,7 @@ public class SquareTests {
   @Test
   public void testArea(){
     Square s = new Square(5);
-    Assert.assertEquals(s.area(), 20);
+    Assert.assertEquals(s.area(), 23);
   }
 
 }
